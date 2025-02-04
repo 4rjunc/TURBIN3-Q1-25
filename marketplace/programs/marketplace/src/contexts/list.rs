a21f0f4756d4817ab3ece148d1383790329453b1
4rjunc <arjunmundamani@gmail.com>
2025-02-04 10:13:26 +0530

    completed Marketplace

--- marketplace/programs/marketplace/src/contexts/list.rs
+++ marketplace/programs/marketplace/src/contexts/list.rs
@@ -1,54 +1,98 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{MasterEditionAccount, Metadata, MetadataAccount},
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+};
 
-use crate::{state::Marketplace, MarketplaceError};
+use crate::state::{Listing, Marketplace};
 
 #[derive(Accounts)]
-#[instruction(name: String)]
-pub struct Initialize<'info> {
+pub struct List<'info> {
     #[account(mut)]
-    pub admin: Signer<'info>,
+    pub maker: Signer<'info>,
     #[account(
-        init,
-        payer = admin,
-        seeds = [b"marketplace", name.as_str().as_bytes()],
-        bump,
-        space = Marketplace::INIT_SPACE
+        seeds = [b"marketplace", marketplace.name.as_str().as_bytes()],
+        bump = marketplace.bump,
     )]
     pub marketplace: Account<'info, Marketplace>,
+    pub maker_mint: InterfaceAccount<'info, Mint>,
     #[account(
-        seeds = [b"treasury", marketplace.key().as_ref()],
-        bump,
+        mut,
+        associated_token::mint = maker_mint,
+        associated_token::authority = maker,
     )]
-    pub treasury: SystemAccount<'info>,
+    pub maker_ata: InterfaceAccount<'info, TokenAccount>,
     #[account(
         init,
-        payer = admin,
-        seeds = [b"rewards", marketplace.key().as_ref()],
-        bump,
-        mint::decimals = 6,
-        mint::authority = marketplace,
+        payer = maker,
+        associated_token::mint = maker_mint,
+        associated_token::authority = listing,
     )]
-    pub rewards_mint: InterfaceAccount<'info, Mint>,
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = maker,
+        seeds = [marketplace.key().as_ref(), maker_mint.key().as_ref()],
+        bump,
+        space = Listing::INIT_SPACE,
+    )]
+    pub listing: Account<'info, Listing>,
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        seeds = [
+            b"metadata", 
+            metadata_program.key().as_ref(),
+            maker_mint.key().as_ref(),
+        ],
+        seeds::program = metadata_program.key(),
+        bump,
+        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().verified == true,
+    )]
+    pub metadata: Account<'info, MetadataAccount>,
+    #[account(
+        seeds = [
+            b"metadata", 
+            metadata_program.key().as_ref(),
+            maker_mint.key().as_ref(),
+            b"edition"
+        ],
+        seeds::program = metadata_program.key(),
+        bump,
+    )]
+    pub master_edition: Account<'info, MasterEditionAccount>,
+    pub metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
-impl<'info> Initialize<'info> {
-    pub fn init(&mut self, name: String, fee: u16, bumps: &InitializeBumps) -> Result<()> {
-        require!(
-            name.len() > 0 && name.len() < 4 + 33,
-            MarketplaceError::NameTooLong
-        );
-
-        self.marketplace.set_inner(Marketplace {
-            admin: self.admin.key(),
-            fee,
-            bump: bumps.marketplace,
-            treasury_bump: bumps.treasury,
-            rewards_bump: bumps.rewards_mint,
-            name,
+impl<'info> List<'info> {
+    pub fn create_listing(&mut self, price: u64, bumps: &ListBumps) -> Result<()> {
+        self.listing.set_inner(Listing {
+            maker: self.maker.key(),
+            mint: self.maker_mint.key(),
+            price,
+            bump: bumps.listing,
         });
+
+        Ok(())
+    }
+
+    pub fn deposit_nft(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.maker_ata.to_account_info(),
+            to: self.vault.to_account_info(),
+            authority: self.maker.to_account_info(),
+            mint: self.maker_mint.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_ctx, 1, self.maker_mint.decimals)?;
 
         Ok(())
     }
