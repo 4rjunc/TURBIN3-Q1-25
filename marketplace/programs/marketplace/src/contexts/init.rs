b2447f297db6fbe2b466eabf90dd901d85bb043f
4rjunc <arjunmundamani@gmail.com>
2025-02-04 08:56:09 +0530

    Context Listing

--- marketplace/programs/marketplace/src/contexts/init.rs
+++ marketplace/programs/marketplace/src/contexts/init.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface};
 
-use crate::state::Marketplace;
+use crate::{state::Marketplace, MarketplaceError};
 
 #[derive(Accounts)]
 #[instruction(name: String)]
@@ -16,8 +17,39 @@ pub struct Initialize<'info> {
     )]
     pub marketplace: Account<'info, Marketplace>,
     #[account(
-        seeds = [b"treasury", marketplace.key().as_ref()]
+        seeds = [b"treasury", marketplace.key().as_ref()],
         bump,
     )]
     pub treasury: SystemAccount<'info>,
+    #[account(
+        init,
+        payer = admin,
+        seeds = [b"rewards", marketplace.key().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = marketplace,
+    )]
+    pub rewards_mint: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn init(&mut self, name: String, fee: u16, bumps: &InitializeBumps) -> Result<()> {
+        require!(
+            name.len() > 0 && name.len() < 4 + 33,
+            MarketplaceError::NameTooLong
+        );
+
+        self.marketplace.set_inner(Marketplace {
+            admin: self.admin.key(),
+            fee,
+            bump: bumps.marketplace,
+            treasury_bump: bumps.treasury,
+            rewards_bump: bumps.rewards_mint,
+            name,
+        });
+
+        Ok(())
+    }
 }
