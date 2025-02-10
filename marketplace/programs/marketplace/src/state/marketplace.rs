d947fbabad9395ea9d236beb77e020f9acbfe8eb
4rjunc <arjunmundamani@gmail.com>
2025-02-10 10:15:53 +0530

    spaces in marketplace

--- marketplace/programs/marketplace/src/state/marketplace.rs
+++ marketplace/programs/marketplace/src/state/marketplace.rs
@@ -1,15 +1,17 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct Marketplace {
     pub admin: Pubkey,
     pub fee: u16,
     pub bump: u8,
     pub treasury_bump: u8,
     pub rewards_bump: u8,
+    #[max_len(20)]
     pub name: String, // Set the limit to 32 bytes
 }
 
-impl Space for Marketplace {
-    const INIT_SPACE: usize = 8 + 32 + 2 + 1 + 1 + 1 + (4 + 32);
-}
+//impl Space for Marketplace {
+//    const INIT_SPACE: usize = 8 + 32 + 2 + 1 + 1 + 1 + (4 + 32);
+//}
