c25d736a7be0be9d6eea59df28cd43014a271d4c
4rjunc <arjunmundamani@gmail.com>
2025-02-11 15:13:21 +0530

    Erro and Lib

--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -1,16 +1,46 @@
 use anchor_lang::prelude::*;
 
+pub mod state;
+pub use state::*;
+
+pub mod context;
+pub use context::*;
+
+pub mod errors;
+
 declare_id!("7MFsykToLeEHYmfsxid2d7Fc9K1LApTB2y7QnGmfoZXX");
 
 #[program]
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, claim_amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(claim_amount, max_x, max_y)?;
+        Ok(())
+    }
+
+    pub fn withdraw(
+        ctx: Context<Withdraw>,
+        claim_amount: u64,
+        min_x: u64,
+        min_y: u64,
+    ) -> Result<()> {
+        ctx.accounts.withdraw(claim_amount, min_x, min_y)?;
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
